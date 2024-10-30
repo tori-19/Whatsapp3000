f1e9feaaec854c9752a5317019a0e359b6cedc68
tmorweng023 <tmorweng023@student.wethinkcode.co.za>
2024-10-30 07:40:11 +0200

    addedfunctions to handle input from the user

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import pywhatkit as kit
 
-kit.sendwhatmsg("+27605077578", "Hello", 7, 40)
+def get_number():
+    """Get the WhatsApp number with country code."""
+    return input("Enter the phone number (with country code, e.g., +27605077578): ")
+
+def get_msg():
+    """Get the message to send."""
+    return input("Enter the message to send: ")
+
+def get_time():
+    """Get the hour and minute to send the message."""
+    hour = int(input("Enter the hour (24-hour format, e.g., 14 for 2 PM): "))
+    minute = int(input("Enter the minute: "))
+    return hour, minute
+
+# Collect information from the user
+number = get_number()
+message = get_msg()
+hour, minute = get_time()
+
+# Send the message
+kit.sendwhatmsg(number, message, hour, minute)
